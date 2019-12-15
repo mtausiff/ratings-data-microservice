5a50a1070d08db9ac71435227ebc9eefba915eb5
mtausiff <tausifcse17@gmail.com>
2019-12-15 21:21:46 +0300

    Initial ratings-data-microservice project commit

---
+++ ratings-data-microservice/src/main/java/com/ratings/data/microservice/RatingsDataMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ratings.data.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataMicroserviceApplication.class, args);
+	}
+
+}
---
+++ ratings-data-microservice/src/test/java/com/ratings/data/microservice/RatingsDataMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratings.data.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
